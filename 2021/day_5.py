e39ecb717224a30aa39169bccaa97a46c2d899b9
Sean McPherson <git@seanmcp.com>
2021-12-06 22:43:51 -0500

    feat: 2021.5.2

--- 2021/day_5.py
+++ 2021/day_5.py
@@ -16,6 +16,12 @@ input = list(map(to_coordinates, input))
 grid = dict()
 greater_than_two = []
 
+def sort_set(set):
+    if set[0][0] < set[1][0]:
+        return set
+    else:
+        return [set[1], set[0]]
+
 def get_range(a, b):
     return range(a if a < b else b, (b if a < b else a) + 1)
 
@@ -23,9 +29,18 @@ def get_coord(x, y):
     return str(x) + ',' + str(y)
 
 for set in input:
-    # Ignore diagonal segments
+    # Diagonal segments
     if set[0][0] != set[1][0] and set[0][1] != set[1][1]:
-        continue
+        sorted = sort_set(set)
+        multiplier = 1 if sorted[0][1] < sorted[1][1] else -1
+        for i, x in enumerate(get_range(sorted[0][0], sorted[1][0])):
+            coord = get_coord(x, sorted[0][1] + i * multiplier)
+            if coord in grid:
+                grid[coord] = grid[coord] + 1
+                if grid[coord] == 2:
+                    greater_than_two.append(coord)
+            else:
+                grid[coord] = 1
 
     if set[0][0] == set[1][0]:
         # Iterate y
@@ -48,4 +63,5 @@ for set in input:
             else:
                 grid[coord] = 1
 
-print('part 1:', len(greater_than_two))
+# print('part 1:', len(greater_than_two))
+print('part 2:', len(greater_than_two))
